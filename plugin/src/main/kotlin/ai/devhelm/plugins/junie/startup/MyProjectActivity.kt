03787547200b88af50780ea15a90c91aed8d721c
Marcus Vance <227830304+marcus-vance@users.noreply.github.com>
2025-08-23 15:30:55 +0200

    DH-NIL: Plugin logs and notifies when Junie just finished (add notification group, timestamped message)

--- plugin/src/main/kotlin/ai/devhelm/plugins/junie/startup/MyProjectActivity.kt
+++ plugin/src/main/kotlin/ai/devhelm/plugins/junie/startup/MyProjectActivity.kt
@@ -1,12 +1,28 @@
 package ai.devhelm.plugins.junie.startup
 
+import com.intellij.notification.NotificationType
+import com.intellij.notification.NotificationGroupManager
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class MyProjectActivity : ProjectActivity {
 
     override suspend fun execute(project: Project) {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        val finishedAt = ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        // Primary requirement: log that Junie just finished
+        thisLogger().info("Junie just finished for project='${'$'}{project.name}' at ${'$'}finishedAt")
+
+        // Extra work: show a user notification balloon with timestamp
+        NotificationGroupManager.getInstance()
+            .getNotificationGroup("Junie Notifications")
+            .createNotification(
+                "Junie finished",
+                "Junie just finished for project '${'$'}{project.name}' at ${'$'}finishedAt",
+                NotificationType.INFORMATION
+            )
+            .notify(project)
     }
 }
