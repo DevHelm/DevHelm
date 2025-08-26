bc77dc8e9d753fffe2433ee38d6e034ce4ab82bd
Marcus Vance <227830304+marcus-vance@users.noreply.github.com>
2025-08-26 20:41:04 +0200

    DH-21 Allow API to use agent as a user

--- web/src/Repository/ApiKeyRepositoryInterface.php
+++ web/src/Repository/ApiKeyRepositoryInterface.php
@@ -6,4 +6,11 @@ use Parthenon\Athena\Repository\CrudRepositoryInterface;
 
 interface ApiKeyRepositoryInterface extends CrudRepositoryInterface
 {
+    /**
+     * Find an enabled API key by its key value.
+     *
+     * @param string $key The API key to look for
+     * @return \App\Entity\ApiKey|null The API key entity if found and enabled, null otherwise
+     */
+    public function findEnabledByKey(string $key): ?\App\Entity\ApiKey;
 }
